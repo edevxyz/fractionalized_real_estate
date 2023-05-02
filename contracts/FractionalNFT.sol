6d07600f3b8d18723726be1da3a92fdd9575efa1
Your Name <eli@MSI.localdomain>
2023-05-02 22:06:28 +0200

    importsv2

--- contracts/FractionalNFT.sol
+++ contracts/FractionalNFT.sol
@@ -1,8 +1,6 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/interfaces/IERC2981.sol";
+import "@openzeppelin/contracts/token/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract FractionalNFT is ERC721URIStorage, IERC2981 {
-    using Counters for Counters.Counter;
