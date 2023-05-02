ccb8c347a211c1be2ac96ce73d84e9ed7d251495
Your Name <eli@MSI.localdomain>
2023-05-03 01:06:54 +0200

    correct name and symbol

--- test/FractionalNFT.test.js
+++ test/FractionalNFT.test.js
@@ -5,7 +5,7 @@ describe("FractionalRealEstate", function () {
   let FractionalRealEstate, fractionalRealEstate, owner, addr1, addr2
 
   beforeEach(async () => {
-    FractionalRealEstate = await ethers.getContractFactory("FractionalRealEstate")
+  //  FractionalRealEstate = await ethers.getContractFactory("FractionalRealEstate")
     [owner, addr1, addr2] = await ethers.getSigners()
     fractionalRealEstate = await FractionalRealEstate.deploy()
   })
@@ -14,3 +14,9 @@ describe("Deployment", function () {
     it("Should set the right owner", async function () {
       expect(await fractionalRealEstate.owner()).to.equal(owner.address)
     })
+
+it("Should initialize with the correct token name and symbol", async function () {
+      expect(await fractionalRealEstate.name()).to.equal("FractionalRealEstate")
+      expect(await fractionalRealEstate.symbol()).to.equal("FRE")
+    })
+  })
