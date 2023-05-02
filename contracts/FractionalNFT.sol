817cc181ee7114028709f52f840b6dcf0faae50f
Your Name <eli@MSI.localdomain>
2023-05-02 22:12:06 +0200

    foundation

--- contracts/FractionalNFT.sol
+++ contracts/FractionalNFT.sol
@@ -4,3 +4,9 @@ import "@openzeppelin/contracts/token/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+contract FractionalRealEstate is ERC721, Ownable {
+using Counters for Counters.Counter;
+Counters.Counter private _tokenIds;
+uint256 public constant MAX_SUPPLY = 1000;
+uint256 public constant MINT_PRICE = 0.1 ether;
+mapping (uint256 => string) private _tokenImages; // Added mapping to store IPFS hash for each token
