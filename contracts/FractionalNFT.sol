2e84194ccb43f3009f26939f019463e03402e40d
Your Name <eli@MSI.localdomain>
2023-05-03 00:01:04 +0200

    FractionalRealEstate

--- contracts/FractionalNFT.sol
+++ contracts/FractionalNFT.sol
@@ -1,6 +1,7 @@
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
@@ -10,3 +11,7 @@ Counters.Counter private _tokenIds;
 uint256 public constant MAX_SUPPLY = 1000;
 uint256 public constant MINT_PRICE = 0.1 ether;
 mapping (uint256 => string) private _tokenImages; // Added mapping to store IPFS hash for each token
+
+constructor() ERC721("FractionalRealEstate", "FRE") {}
+
+}
--- test/FractionalNFT.test.js
+++ test/FractionalNFT.test.js
@@ -1,2 +1,11 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
+
+describe("FractionalRealEstate", function () {
+  let FractionalRealEstate, fractionalRealEstate, owner, addr1, addr2
+
+  beforeEach(async () => {
+    FractionalRealEstate = await ethers.getContractFactory("FractionalRealEstate")
+    [owner, addr1, addr2] = await ethers.getSigners()
+    fractionalRealEstate = await FractionalRealEstate.deploy()
+  })
