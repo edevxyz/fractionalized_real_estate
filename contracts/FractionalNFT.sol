a1b9658d4e69a10f1b7c75c58325dcac642a6be1
Your Name <eli@MSI.localdomain>
2023-05-21 14:17:24 +0200

    beautifying and adding comments into files

--- contracts/FractionalNFT.sol
+++ contracts/FractionalNFT.sol
@@ -1,60 +1,70 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Importing required OpenZeppelin contracts
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+// FractionalRealEstate contract inherits from ERC721 and Ownable
 contract FractionalRealEstate is ERC721, Ownable {
-using Counters for Counters.Counter;
-Counters.Counter private _tokenIds;
-uint256 public constant MAX_SUPPLY = 1000;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds; // Counter for token IDs
+    uint256 public constant MAX_SUPPLY = 1000; // Maximum supply of tokens
 
-function totalSupply() public view returns (uint256) {
-    return _tokenIds.current();   
-}
-
-constructor() ERC721("FractionalRealEstate", "FRE") {}
-
-function mint(string memory imageIPFSHash) public payable {
-    require(totalSupply() < MAX_SUPPLY, "Maximum supply reached");
-    require(msg.value == 0.1 ether, "Incorrect price");
-
-    _tokenIds.increment();
-    uint256 newTokenId = _tokenIds.current();
-    _tokenImages[newTokenId] = imageIPFSHash; // Stored IPFS hash in _tokenImages mapping
-    _mint(msg.sender, newTokenId);
-}
-
-mapping (uint256 => string) private _tokenImages; // Added mapping to store IPFS hash for each token
-
-function tokenURI(uint256 tokenId) public view override returns (string memory) {
-    require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
-
-    string memory baseURI = "https://ipfs.io/ipfs/";
-    string memory tokenImageHash = _tokenImages[tokenId];
-    return string(abi.encodePacked(baseURI, tokenImageHash));
-}
-
-function withdraw() public onlyOwner {
-    uint256 balance = address(this).balance;
-    payable(owner()).transfer(balance);  
-}
-function setTokenImages(string memory folderIPFSHash) public onlyOwner {
-    for (uint256 i = 1; i <= MAX_SUPPLY; i++) {
-        _tokenImages[i] = folderIPFSHash;
+    // Returns the total supply of tokens
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds.current();
     }
-}
 
-function payRent() public payable {
-    uint256 totalTokens = totalSupply();
-    require(totalTokens > 0, "No tokens have been minted yet");
-    uint256 rentPerToken = msg.value / totalTokens;
-    for (uint256 i = 1; i <= totalTokens; i++) {
-        address tokenHolder = ownerOf(i);
-        uint256 rentOwed = balanceOf(tokenHolder) * rentPerToken;
-        payable(tokenHolder).transfer(rentOwed);
+    // Constructor to set the token name and symbol
+    constructor() ERC721("FractionalRealEstate", "FRE") {}
+
+    // Function to mint a new token with the given IPFS hash
+    function mint(string memory imageIPFSHash) public payable {
+        require(totalSupply() < MAX_SUPPLY, "Maximum supply reached");
+        require(msg.value == 0.1 ether, "Incorrect price");
+
+        _tokenIds.increment(); // Increment the token ID counter
+        uint256 newTokenId = _tokenIds.current(); // Get the new token ID
+        _tokenImages[newTokenId] = imageIPFSHash; // Store IPFS hash in _tokenImages mapping
+        _mint(msg.sender, newTokenId); // Mint the new token
     }
-}
 
+    // Mapping to store IPFS hash for each token
+    mapping (uint256 => string) private _tokenImages;
+
+    // Function to return the token URI for a given token ID
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
+
+        string memory baseURI = "https://ipfs.io/ipfs/";
+        string memory tokenImageHash = _tokenImages[tokenId];
+        return string(abi.encodePacked(baseURI, tokenImageHash));
+    }
+
+    // Function to withdraw the contract balance to the owner
+    function withdraw() public onlyOwner {
+        uint256 balance = address(this).balance;
+        payable(owner()).transfer(balance);
+    }
+
+    // Function to set the IPFS hash for all tokens
+    function setTokenImages(string memory folderIPFSHash) public onlyOwner {
+        for (uint256 i = 1; i <= MAX_SUPPLY; i++) {
+            _tokenImages[i] = folderIPFSHash;
+        }
+    }
+
+    // Function to distribute rent payments to token holders
+    function payRent() public payable {
+        uint256 totalTokens = totalSupply();
+        require(totalTokens > 0, "No tokens have been minted yet");
+        uint256 rentPerToken = msg.value / totalTokens;
+        for (uint256 i = 1; i <= totalTokens; i++) {
+            address tokenHolder = ownerOf(i);
+            uint256 rentOwed = balanceOf(tokenHolder) * rentPerToken;
+            payable(tokenHolder).transfer(rentOwed);
+        }
+    }
 }
--- test/FractionalNFT.test.js
+++ test/FractionalNFT.test.js
@@ -1,37 +1,46 @@
 const { expect } = require("chai")
 const { ethers } = require("hardhat")
 
+// Test suite for the FractionalRealEstate contract
 describe("FractionalRealEstate", function () {
   let FractionalRealEstate, fractionalRealEstate, owner, addr1, addr2
 
+  // Set up the contract instance and signers before each test
   beforeEach(async () => {
     FractionalRealEstate = await ethers.getContractFactory("FractionalRealEstate");
     [owner, addr1, addr2] = await ethers.getSigners()
     fractionalRealEstate = await FractionalRealEstate.deploy()
   })
 
-describe("Deployment", function () {
+  // Test suite for deployment-related functionality
+  describe("Deployment", function () {
+    // Test if the contract is deployed with the correct owner
     it("Should set the right owner", async function () {
       expect(await fractionalRealEstate.owner()).to.equal(owner.address)
     })
 
-it("Should initialize with the correct token name and symbol", async function () {
+    // Check if the contract is deployed with the correct token name and symbol
+    it("Should initialize with the correct token name and symbol", async function () {
       expect(await fractionalRealEstate.name()).to.equal("FractionalRealEstate")
       expect(await fractionalRealEstate.symbol()).to.equal("FRE")
     })
   })
 
- describe("Minting", function () {
+  // Test suite for minting-related functionality
+  describe("Minting", function () {
+    // Test if a token is minted successfully and the IPFS hash is stored
     it("Should mint a token successfully and store the IPFS hash", async function () {
       await fractionalRealEstate.connect(addr1).mint("QmHash1", { value: ethers.utils.parseEther("0.1") })
       expect(await fractionalRealEstate.totalSupply()).to.equal(1)
       expect(await fractionalRealEstate.tokenURI(1)).to.equal("https://ipfs.io/ipfs/QmHash1")
     })
 
-it("Should revert if minting with incorrect price", async function () {
+    // Test if minting reverts when the price is incorrect
+    it("Should revert if minting with incorrect price", async function () {
       await expect(fractionalRealEstate.connect(addr1).mint("QmHash2", { value: ethers.utils.parseEther("0.05") })).to.be.revertedWith("Incorrect price")
     })
 
+    // Test if minting reverts when the maximum supply is reached
     it("Should revert if maximum supply is reached", async function () {
       for (let i = 1; i <= 1000; i++) {
         await fractionalRealEstate.connect(addr1).mint(`QmHash${i}`, { value: ethers.utils.parseEther("0.1") })
@@ -40,27 +49,33 @@ it("Should revert if minting with incorrect price", async function () {
     })
   })
 
-describe("Withdraw function", function () {
+  // Test suite for the withdraw function
+  describe("Withdraw function", function () {
+    // Test if the owner can withdraw the contract balance
     it("Should allow the owner to withdraw the contract balance", async function () {
-    await fractionalRealEstate.connect(addr1).mint("QmTestImageHash1", { value: ethers.utils.parseEther("0.1") })
-    const initialOwnerBalance = await owner.getBalance()
-    const contractBalance = await ethers.provider.getBalance(fractionalRealEstate.address)
+      await fractionalRealEstate.connect(addr1).mint("QmTestImageHash1", { value: ethers.utils.parseEther("0.1") })
+      const initialOwnerBalance = await owner.getBalance()
+      const contractBalance = await ethers.provider.getBalance(fractionalRealEstate.address)
 
-    const tx = await fractionalRealEstate.withdraw()
-    const receipt = await tx.wait()
+      const tx = await fractionalRealEstate.withdraw()
+      const receipt = await tx.wait()
 
-    const gasUsed = receipt.gasUsed.mul(tx.gasPrice)
-    const finalOwnerBalance = await owner.getBalance()
+      const gasUsed = receipt.gasUsed.mul(tx.gasPrice)
+      const finalOwnerBalance = await owner.getBalance()
 
-    expect(finalOwnerBalance).to.equal(initialOwnerBalance.add(contractBalance).sub(gasUsed))
-  })
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance.add(contractBalance).sub(gasUsed))
+    })
+
+    // Test if a non-owner is prevented from withdrawing
     it("Should revert if a non-owner tries to withdraw", async function () {
       await fractionalRealEstate.connect(addr1).mint("QmHash1", { value: ethers.utils.parseEther("0.1") })
       await expect(fractionalRealEstate.connect(addr1).withdraw()).to.be.revertedWith("Ownable: caller is not the owner")
     })
-  })    
+  })
 
-describe("Set Token Images", function () {
+  // Test suite for setting token images
+  describe("Set Token Images", function () {
+    // Test if the owner can set token images when not all tokens are minted
     it("Should set token images when called by the owner (not only when everything has been minted)", async function () {
       for (let i = 1; i <= 1000; i++) {
         await fractionalRealEstate.connect(addr1).mint(`QmHash${i}`, { value: ethers.utils.parseEther("0.1") })
@@ -71,64 +86,67 @@ describe("Set Token Images", function () {
       }
     })
 
- it("Should revert if called by a non-owner", async function () {
-       await expect(fractionalRealEstate.connect(addr1).setTokenImages("QmFolderHash")).to.be.revertedWith("Ownable: caller is not the owner")
-     })
-
-// uncomment this to  see the test fail to test whether all tokens do not have to be minted to call
-//it("Should revert if all tokens are not minted", async function () {
-//      await fractionalRealEstate.connect(addr1).mint("QmHash1", { value: ethers.utils.parseEther("0.1") })
-//      await expect(fractionalRealEstate.setTokenImages("QmFolderHash")).to.be.revertedWith("All tokens must be minted before setting images")
-//    })
+    // Test if a non-owner is prevented from setting token images
+    it("Should revert if called by a non-owner", async function () {
+      await expect(fractionalRealEstate.connect(addr1).setTokenImages("QmFolderHash")).to.be.revertedWith("Ownable: caller is not the owner")
+    })
   })
-describe("payRent", function() {
-it("Should distribute rent correctly among token holders", async function () {
-  // Mint tokens for addr1 and addr2
-  await fractionalRealEstate.connect(addr1).mint("QmHash1", { value: ethers.utils.parseEther("0.1") })
-  await fractionalRealEstate.connect(addr2).mint("QmHash2", { value: ethers.utils.parseEther("0.1") })
 
-  // Check initial balances
-  const initialBalance1 = await addr1.getBalance()
-  const initialBalance2 = await addr2.getBalance()
+  // Test suite for payRent function
+  describe("payRent", function() {
+    // Test if rent is distributed correctly among token holders
+    it("Should distribute rent correctly among token holders", async function () {
+      // Mint tokens for addr1 and addr2
+      await fractionalRealEstate.connect(addr1).mint("QmHash1", { value: ethers.utils.parseEther("0.1") })
+      await fractionalRealEstate.connect(addr2).mint("QmHash2", { value: ethers.utils.parseEther("0.1") })
 
-  // Pay rent
-  const rentAmount = ethers.utils.parseEther("1");
-  await fractionalRealEstate.connect(owner).payRent({ value: rentAmount })
+      // Check initial balances
+      const initialBalance1 = await addr1.getBalance()
+      const initialBalance2 = await addr2.getBalance()
 
-  // Check final balances
-  const finalBalance1 = await addr1.getBalance()
-  const finalBalance2 = await addr2.getBalance()
+      // Pay rent
+      const rentAmount = ethers.utils.parseEther("1")
+      await fractionalRealEstate.connect(owner).payRent({ value: rentAmount })
 
-  // Verify that the rent was distributed correctly
-  expect(finalBalance1).to.equal(initialBalance1.add(rentAmount.div(2)))
-  expect(finalBalance2).to.equal(initialBalance2.add(rentAmount.div(2)))
-})
+      // Check final balances
+      const finalBalance1 = await addr1.getBalance()
+      const finalBalance2 = await addr2.getBalance()
 
-it("Should not allow rent payment when no tokens have been minted", async function () {
-  const rentAmount = ethers.utils.parseEther("1")
-  await expect(fractionalRealEstate.connect(owner).payRent({ value: rentAmount })).to.be.revertedWith("No tokens have been minted yet")
-})
+      // Verify that the rent was distributed correctly
+      expect(finalBalance1).to.equal(initialBalance1.add(rentAmount.div(2)))
+      expect(finalBalance2).to.equal(initialBalance2.add(rentAmount.div(2)))
+    })
 
+    // Test if rent payment is not allowed when no tokens have been minted
+    it("Should not allow rent payment when no tokens have been minted", async function () {
+      const rentAmount = ethers.utils.parseEther("1")
+      await expect(fractionalRealEstate.connect(owner).payRent({ value: rentAmount })).to.be.revertedWith("No tokens have been minted yet")
+    })
 
-it("Should distribute excess rent payment proportionally among token holders", async function () {
-  await fractionalRealEstate.connect(addr1).mint("QmHash1", { value: ethers.utils.parseEther("0.1") })
-  await fractionalRealEstate.connect(addr2).mint("QmHash2", { value: ethers.utils.parseEther("0.1") })
+    // Test if excess  rent payment is distributed proportionally among token holders
+    it("Should distribute excess rent payment proportionally among token holders", async function () {
+      // Mint tokens for addr1 and addr2
+      await fractionalRealEstate.connect(addr1).mint("QmHash1", { value: ethers.utils.parseEther("0.1") })
+      await fractionalRealEstate.connect(addr2).mint("QmHash2", { value: ethers.utils.parseEther("0.1") })
 
-  const initialBalance1 = await addr1.getBalance()
-  const initialBalance2 = await addr2.getBalance()
+      // Check initial balances
+      const initialBalance1 = await addr1.getBalance()
+      const initialBalance2 = await addr2.getBalance()
 
-  const excessRentAmount = ethers.utils.parseEther("1.5");
-  await fractionalRealEstate.connect(owner).payRent({ value: excessRentAmount })
+      // Pay excess rent
+      const excessRentAmount = ethers.utils.parseEther("1.5")
+      await fractionalRealEstate.connect(owner).payRent({ value: excessRentAmount })
 
-  const finalBalance1 = await addr1.getBalance()
-  const finalBalance2 = await addr2.getBalance()
+      // Check final balances
+      const finalBalance1 = await addr1.getBalance()
+      const finalBalance2 = await addr2.getBalance()
 
-  const expectedRentPerToken = excessRentAmount.div(2)
-  expect(finalBalance1).to.equal(initialBalance1.add(expectedRentPerToken))
-  expect(finalBalance2).to.equal(initialBalance2.add(expectedRentPerToken))
-})
-})
+      // Calculate expected rent per token
+      const expectedRentPerToken = excessRentAmount.div(2)
 
-
-
+      // Verify that the excess rent was distributed correctly
+      expect(finalBalance1).to.equal(initialBalance1.add(expectedRentPerToken))
+      expect(finalBalance2).to.equal(initialBalance2.add(expectedRentPerToken))
+    })
+  })
 })
